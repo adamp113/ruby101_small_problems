7d8d9f8142761351689b8039bcc7f6a6dacd4dea
Adam Peterson <AdamP113@gmail.com>
2020-06-11 10:04:34 -0400

    easy 2 first commit

---
+++ easy_2.rb
@@ -0,0 +1,13 @@
+#EASY 2 QUESTIONS
+
+# Title 	Status
+# 1 	How old is Teddy? 	completed
+# 2 	How big is the room? 	completed
+# 3 	Tip calculator 	completed
+# 4 	When will I Retire? 	completed
+# 5 	Greeting a user 	completed
+# 6 	Odd Numbers 	completed
+# 7 	Even Numbers 	completed
+# 8 	Sum or Product of Consecutive Integers 	completed
+# 9 	String Assignment 	completed
+# 10 	Mutation 	completed
