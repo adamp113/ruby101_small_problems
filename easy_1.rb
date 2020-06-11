5428242d1a31496cc76619cf249d624e5e92ec83
Adam Peterson <AdamP113@gmail.com>
2020-06-10 23:24:10 -0400

    All of Easy 1 SPEs

--- easy_1.rb
+++ easy_1.rb
@@ -12,7 +12,7 @@
 # 10 	What's my Bonus? 	completed
 
 
-# Repeat Yourself
+# 1.1: REPEAT YOURSELF
 
 # Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
 
@@ -33,3 +33,274 @@ def repeat(string, num)
 end
 
 repeat('Hello', 3)
+
+
+# 1.2: ODD
+
+# Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+
+# Examples:
+
+# puts is_odd?(2)    # => false
+# puts is_odd?(5)    # => true
+# puts is_odd?(-17)  # => true
+# puts is_odd?(-8)   # => false
+# puts is_odd?(0)    # => false
+# puts is_odd?(7)    # => true
+
+# Keep in mind that you're not allowed to use #odd? or #even? in your solution.
+
+def is_odd?(num)
+  num % 2 != 0
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
+
+
+# 1.3: LIST OF DIGITS
+
+# Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
+
+# Examples:
+
+# puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+# puts digit_list(7) == [7]                     # => true
+# puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+# puts digit_list(444) == [4, 4, 4]             # => true
+
+def digit_list(integer)
+  integer.digits.reverse
+end
+
+def digit_list(integer)
+  integer.to_s.chars.map {|char| char.to_i}
+end
+
+#shorthand for above
+def digit_list(integer)
+  integer.to_s.chars.map(&:to_i)
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
+
+
+# 1.4: HOW MANY?
+
+# Write a method that counts the number of occurrences of each element in a given array.
+
+# vehicles = [
+#   'car', 'car', 'truck', 'car', 'SUV', 'truck',
+#   'motorcycle', 'motorcycle', 'car', 'truck'
+# ]
+
+# count_occurrences(vehicles)
+
+# The words in the array are case-sensitive: 'suv' != 'SUV'. ` Once counted, print each element alongside the number of occurrences.
+
+# Expected output:
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
+
+def count_occurrences(list)
+  occurrences = Hash.new(0)
+  list.each do |vehicle|
+    occurrences[vehicle] += 1
+  end
+  occurrences.each do |vehicle, number|
+    puts "#{vehicle} => #{number}"
+  end
+end
+
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+count_occurrences(vehicles)
+
+
+
+# 1.5: REVERSE IT (PART 1)
+
+# Write a method that takes one argument, a string, and returns a new string with the words in reverse order.
+
+# Examples:
+
+# puts reverse_sentence('') == ''
+# puts reverse_sentence('Hello World') == 'World Hello'
+# puts reverse_sentence('Reverse these words') == 'words these Reverse'
+
+# The tests above should print true.
+
+def reverse_sentence(string)
+  string.split.reverse.join(' ')
+end
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+
+
+# 1.6: REVERSE IT (PART 2)
+
+# Write a method that takes one argument, a string containing one or more words, and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
+
+# Examples:
+
+# puts reverse_words('Professional')          # => lanoisseforP
+# puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+# puts reverse_words('Launch School')         # => hcnuaL loohcS
+
+def reverse_words(string)
+  final = string.split.map do |word|
+    if word.length >= 5
+      word.reverse
+    else
+      word
+    end
+  end
+  final.join(' ')
+end
+
+def reverse_words(string)
+  words = []
+
+  string.split.each do |word|
+    word.reverse! if word.length >= 5
+    words << word
+  end
+  words.join(' ')
+end
+
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
+
+
+
+# 1.7: STRINGY STRINGS
+
+# Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
+
+# Examples:
+
+# puts stringy(6) == '101010'
+# puts stringy(9) == '101010101'
+# puts stringy(4) == '1010'
+# puts stringy(7) == '1010101'
+
+def stringy(integer)
+  string = ''
+  count = 0  
+  loop do 
+    break if count == integer
+    count.even? ? string << '1' : string << '0'
+    count += 1
+    end
+
+  string
+end
+
+def stringy(integer)
+  string = ''
+  integer.times do |index|
+    index.even? ? string << '1' : string << '0'
+  end
+
+  string 
+end
+
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
+
+# 1.8: ARRAY AVERAGE
+
+# Write a method that takes one argument, an array containing integers, and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers.
+
+# Examples:
+
+# puts average([1, 5, 87, 45, 8, 8]) == 25
+# puts average([9, 47, 23, 95, 16, 52]) == 40
+
+# The tests above should print true.
+
+
+def average(array)
+  array.sum / array.size
+end
+
+def average(numbers)
+  sum = numbers.reduce { |sum, number| sum + number }
+  sum / numbers.count
+end
+
+#shorthand for above
+# numbers.reduce(:+)
+
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
+
+
+# 1.9: SUM OF DIGITS
+
+# Write a method that takes one argument, a positive integer, and returns the sum of its digits.
+
+# Examples:
+
+# puts sum(23) == 5
+# puts sum(496) == 19
+# puts sum(123_456_789) == 45
+
+# The tests above should print true.
+
+# For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
+
+def sum(integer)
+  integer.to_s.chars.map(&:to_i).sum
+end
+
+def sum(number)
+  number.to_s.chars.map(&:to_i).reduce(:+)
+end
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
+
+
+
+# 1.10: WHAT'S MY BONUS?
+
+# Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+
+# Examples:
+
+# puts calculate_bonus(2800, true) == 1400
+# puts calculate_bonus(1000, false) == 0
+# puts calculate_bonus(50000, true) == 25000
+
+# The tests above should print true.
+
+def calculate_bonus(salary, bonus)
+  bonus ? (salary / 2) : 0
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
+
+
