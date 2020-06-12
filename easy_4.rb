60d23f3c1fad7a085625fee89c41bf594a6e9736
Adam Peterson <AdamP113@gmail.com>
2020-06-11 21:20:01 -0400

    All of Easy 4 SPEs

---
+++ easy_4.rb
@@ -0,0 +1,352 @@
+# EASY 4 SMALL PROBLEMS
+
+# Title 	Status
+# 1 	Short Long Short 	completed
+# 2 	What Century is That? 	completed
+# 3 	Leap Years (Part 1) 	completed
+# 4 	Leap Years (Part 2) 	completed
+# 5 	Multiples of 3 and 5 	completed
+# 6 	Running Totals 	completed
+# 7 	Convert a String to a Number! 	completed
+# 8 	Convert a String to a Signed Number! 	completed
+# 9 	Convert a Number to a String! 	completed
+# 10 	Convert a Signed Number to a String! 	completed
+
+# 1.1: SHORT LONG SHORT
+
+# Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
+
+# Examples:
+
+# short_long_short('abc', 'defgh') == "abcdefghabc"
+# short_long_short('abcde', 'fgh') == "fghabcdefgh"
+# short_long_short('', 'xyz') == "xyz"
+
+def short_long_short(str1, str2)
+  if str1.length > str2.length
+    str2 + str1 + str2
+  else
+    str1 + str2 + str1
+  end
+end
+
+
+p short_long_short('abc', 'defgh') == "abcdefghabc"
+p short_long_short('abcde', 'fgh') == "fghabcdefgh"
+p short_long_short('', 'xyz') == "xyz"
+
+# 4.2: WHAT CENTURY IS THAT?
+
+# Write a method that takes a year as input and returns the century. The return value should be a string that begins with the century number, and ends with st, nd, rd, or th as appropriate for that number.
+
+# New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
+
+# Examples:
+
+# century(2000) == '20th'
+# century(2001) == '21st'
+# century(1965) == '20th'
+# century(256) == '3rd'
+# century(5) == '1st'
+# century(10103) == '102nd'
+# century(1052) == '11th'
+# century(1127) == '12th'
+# century(11201) == '113th'
+
+def century(year)
+  century = (year / 100) + 1
+  century -= 1 if year % 100 == 0
+  century.to_s + century_suffix(century)
+end
+
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
+  end
+end
+
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+# 4.3: LEAP YEARS (PART 1)
+
+# In the modern era under the Gregorian Calendar, leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
+
+# Assume this rule is good for any year greater than year 0. Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year, or false if it is not a leap year.
+
+# leap_year?(2016) == true
+# leap_year?(2015) == false
+# leap_year?(2100) == false
+# leap_year?(2400) == true
+# leap_year?(240000) == true
+# leap_year?(240001) == false
+# leap_year?(2000) == true
+# leap_year?(1900) == false
+# leap_year?(1752) == true
+# leap_year?(1700) == false
+# leap_year?(1) == false
+# leap_year?(100) == false
+# leap_year?(400) == true
+
+def leap_year?(year)
+  if year % 400 == 0
+    true
+  elsif (year % 4 == 0 ) && (year % 100 != 0)
+    return true
+  else
+    return false
+  end
+end
+
+# OR
+
+def leap_year?(year)
+  (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)
+end
+
+p leap_year?(2016) == true
+p leap_year?(2015) == false
+p leap_year?(2100) == false
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == false
+p leap_year?(1) == false
+p leap_year?(100) == false
+p leap_year?(400) == true
+
+# 4.4 LEAP YEARS (PART 2)
+
+# A continuation of the previous exercise.
+
+# The British Empire adopted the Gregorian Calendar in 1752, which was a leap year. Prior to 1752, the Julian Calendar was used. Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+
+# Using this information, update the method from the previous exercise to determine leap years both before and after 1752.
+
+def leap_year?(year)
+  if year <= 1752
+    year % 4 == 0
+  else
+    (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)
+  end
+end
+
+# OR
+
+def leap_year?(year)
+  if year < 1752 && year % 4 == 0
+    true
+  elsif year % 400 == 0
+    true
+  elsif year % 100 == 0
+    false
+  else
+    year % 4 == 0
+  end
+end
+
+p leap_year?(2016) == true
+p leap_year?(2015) == false
+p leap_year?(2100) == false
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == true
+p leap_year?(1) == false
+p leap_year?(100) == true
+p leap_year?(400) == true
+
+# 4.5: MULTIPLES OF 3 AND 5
+
+# Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number, and then computes the sum of those multiples. For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+# You may assume that the number passed in is an integer greater than 1.
+
+# Examples
+
+# multisum(3) == 3
+# multisum(5) == 8
+# multisum(10) == 33
+# multisum(1000) == 234168
+
+def multisum(number)
+  all_num = (1..number).to_a
+  array = all_num.select do |num|
+    num % 3 == 0 || num % 5 == 0
+  end
+  array.sum
+end
+
+# OR
+
+def multiple?(number, divisor)
+  number % divisor == 0
+end
+
+def multisum(max_value)
+  sum = 0
+  1.upto(max_value) do |number|
+    if multiple?(number, 3) || multiple?(number, 5)
+      sum += number
+    end
+  end
+  sum
+end
+
+p multisum(3) == 3
+p multisum(5) == 8
+p multisum(10) == 33
+p multisum(1000) == 234168
+
+# 4.6: RUNNING TOTALS
+
+
+# Write a method that takes an Array of numbers, and returns an Array with the same number of elements, and each element has the running total from the original Array.
+
+# Examples:
+
+def running_total(array)
+  sum = 0
+  array.map {|num| sum += num}
+end
+
+
+p running_total([2, 5, 13]) == [2, 7, 20]
+p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total([3]) == [3]
+p running_total([]) == []
+
+# 4.7: CONVERT A STRING TO A NUMBER!
+
+# The String#to_i method converts a string of numeric characters (including an optional plus or minus sign) to an Integer. String#to_i and the Integer constructor (Integer()) behave similarly. In this exercise, you will create a method that does the same thing.
+
+# Write a method that takes a String of digits, and returns the appropriate number as an integer. You may not use any of the methods mentioned above.
+
+# For now, do not worry about leading + or - signs, nor should you worry about invalid characters; assume all characters will be numeric.
+
+# You may not use any of the standard conversion methods available in Ruby, such as String#to_i, Integer(), etc. Your method should do this the old-fashioned way and calculate the result by analyzing the characters in the string.
+
+DIGITS = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}
+
+def string_to_integer(string)
+  digits = string.chars.map { |char| DIGITS[char] }
+
+  value = 0
+  digits.each { |digit| value = 10 * value + digit }
+  value
+end
+
+p string_to_integer('4321') == 4321
+p string_to_integer('570') == 570
+
+# 4.8: CONVERT A STRING TO A SIGNED NUMBER!
+
+# In the previous exercise, you developed a method that converts simple numeric strings to Integers. In this exercise, you're going to extend that method to work with signed numbers.
+
+# Write a method that takes a String of digits, and returns the appropriate number as an integer. The String may have a leading + or - sign; if the first character is a +, your method should return a positive number; if it is a -, your method should return a negative number. If no sign is given, you should return a positive number.
+
+# You may assume the string will always contain a valid number.
+
+# You may not use any of the standard conversion methods available in Ruby, such as String#to_i, Integer(), etc. You may, however, use the string_to_integer method from the previous lesson.
+
+def string_to_signed_integer(string)
+  case string[0]
+  when '-' then -string_to_integer(string[1..-1])
+  when '+' then string_to_integer(string[1..-1])
+  else          string_to_integer(string)
+  end
+end
+
+string_to_signed_integer('4321') == 4321
+string_to_signed_integer('-570') == -570
+string_to_signed_integer('+100') == 100
+
+# 4.9: CONVERT A NUMBER TO A STRING!
+
+# In the previous two exercises, you developed methods that convert simple numeric strings to signed Integers. In this exercise and the next, you're going to reverse those methods.
+
+# Write a method that takes a positive integer or zero, and converts it to a string representation.
+
+# You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), Kernel#format, etc. Your method should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
+
+def integer_to_string(number)
+  number.digits.reverse.join
+end
+
+#OR
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string(number)
+  result = ''
+  loop do
+    number, remainder = number.divmod(10)
+    result.prepend(DIGITS[remainder])
+    break if number == 0
+  end
+  result
+end
+
+#OR
+DIGITS = [0,1,2,3,4,5,6,7,9]
+
+def integer_to_string(int)
+  int.digits.reverse.map{|e| DIGITS[e]}.join
+end
+#OR
+def integer_to_string(number)
+  number.digits.reverse.join
+end
+#OR
+def integer_to_string(integer)
+  string = ''
+  integer.digits.reverse.join.split().each {|digit| string << digit }
+  string
+end
+
+integer_to_string(4321) == '4321'
+integer_to_string(0) == '0'
+integer_to_string(5000) == '5000'
+
+# 4.10: CONVERT A SIGNED NUMBER TO A STRING!
+
+# In the previous exercise, you developed a method that converts non-negative numbers to strings. In this exercise, you're going to extend that method by adding the ability to represent negative numbers as well.
+
+# Write a method that takes an integer, and converts it to a string representation.
+
+# You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), Kernel#format, etc. You may, however, use integer_to_string from the previous exercise.
+
+def signed_integer_to_string(number)
+  case number <=> 0
+  when -1 then "-#{integer_to_string(-number)}"
+  when +1 then "+#{integer_to_string(number)}"
+  else         integer_to_string(number)
+  end
+end
+
+signed_integer_to_string(4321) == '+4321'
+signed_integer_to_string(-123) == '-123'
+signed_integer_to_string(0) == '0'
