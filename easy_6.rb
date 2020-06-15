a666a3b4e9cf7bd50326d540a02ef688d9d47875
Adam Peterson <AdamP113@gmail.com>
2020-06-15 15:03:21 -0400

    ALL Easy 6 SPEs

---
+++ easy_6.rb
@@ -0,0 +1,293 @@
+# EASY 6 SMALL PROBLEMS
+
+
+# 6.1: CUTE ANGLES
+
+# Write a method that takes a floating point number that represents an angle between 0 and 360 degrees and returns a String that represents that angle in degrees, minutes and seconds. You should use a degree symbol (°) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. A degree has 60 minutes, while a minute has 60 seconds.
+
+# Examples:
+
+# You may use this constant to represent the degree symbol:
+
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
+
+def dms(degrees_float)
+  total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
+end
+
+
+dms(30) == %(30°00'00")
+dms(76.73) == %(76°43'48")
+dms(254.6) == %(254°36'00")
+dms(93.034773) == %(93°02'05")
+dms(0) == %(0°00'00")
+dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+
+# 6.2: DELETE VOWELS
+
+# Write a method that takes an array of strings, and returns an array of the same string values, except with the vowels (a, e, i, o, u) removed.
+
+def remove_vowels(array)
+  array.map { |string| string.delete('aeiouAEIOU') }
+end
+
+#OR
+
+def remove_vowels(array)
+  array.map { |string| string.gsub(/[aeiou]/i,  '') }
+end
+
+remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+# 6.3: FIBONACCI NUMBER LOCATION BY LENGTH
+
+# The Fibonacci series is a series of numbers (1, 1, 2, 3, 5, 8, 13, 21, ...) such that the first 2 numbers are 1 by definition, and each subsequent number is the sum of the two previous numbers. This series appears throughout the natural world.
+
+# Computationally, the Fibonacci series is a very simple series, but the results grow at an incredibly rapid rate. For example, the 100th Fibonacci number is 354,224,848,179,261,915,075 -- that's enormous, especially considering that it takes 6 iterations before it generates the first 2 digit number.
+
+# Write a method that calculates and returns the index of the first Fibonacci number that has the number of digits specified as an argument. (The first Fibonacci number has index 1.)
+
+def find_fibonacci_index_by_length(number_digits)
+  first = 1
+  second = 1
+  index = 2
+
+  loop do
+    index += 1
+    fibonacci = first + second
+    break if fibonacci.to_s.size >= number_digits
+
+    first = second
+    second = fibonacci
+  end
+
+  index
+end
+
+
+find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+find_fibonacci_index_by_length(10) == 45
+find_fibonacci_index_by_length(100) == 476
+find_fibonacci_index_by_length(1000) == 4782
+find_fibonacci_index_by_length(10000) == 47847
+
+# 6.4: REVERSED ARRAYS (PART 1)
+
+# Write a method that takes an Array as an argument, and reverses its elements in place; that is, mutate the Array passed into this method. The return value should be the same Array object.
+
+# You may not use Array#reverse or Array#reverse!.
+
+def reverse!(array)
+  left_index = 0
+  right_index = -1
+
+  while left_index < array.size / 2
+    array[left_index], array[right_index] = array[right_index], array[left_index]
+    left_index += 1
+    right_index -= 1
+  end
+
+  array
+end
+
+list = [1,2,3,4]
+result = reverse!(list)
+result == [4, 3, 2, 1]
+list == [4, 3, 2, 1]
+list.object_id == result.object_id
+
+list = %w(a b e d c)
+reverse!(list) == ["c", "d", "e", "b", "a"]
+list == ["c", "d", "e", "b", "a"]
+
+list = ['abc']
+reverse!(list) == ["abc"]
+list == ["abc"]
+
+list = []
+reverse!(list) == []
+list == []
+
+# 6.5: REVERSED ARRAYS (PART 2)
+
+
+
+# Write a method that takes an Array, and returns a new Array with the elements of the original list in reverse order. Do not modify the original list.
+
+# You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
+
+def reverse2(array)
+  array.each_with_object([]) { |i, new_array| new_array << array[-i] }
+end
+
+# OR
+def reverse(list)
+  new_array = []
+  index = -1
+
+  loop do
+    new_array << list[index]
+
+    break if index * -1 == list.size
+    index -= 1
+  end
+
+new_array  
+end
+
+# OR
+
+def reverse(array)
+  result_array = []
+  array.reverse_each { |element| result_array << element }
+  result_array
+end
+
+
+reverse([1,2,3,4]) == [4,3,2,1]          # => true
+reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+reverse(['abc']) == ['abc']              # => true
+reverse([]) == []                        # => true
+
+list = [1, 3, 2]                      # => [1, 3, 2]
+new_list = reverse(list)              # => [2, 3, 1]
+list.object_id != new_list.object_id  # => true
+list == [1, 3, 2]                     # => true
+new_list == [2, 3, 1]                 # => true
+
+
+
+# 6.6: COMBINING ARRAYS
+
+# Write a method that takes two Arrays as arguments, and returns an Array that contains all of the values from the argument Arrays. There should be no duplication of values in the returned Array, even if there are duplicates in the original Arrays.
+
+def merge(arr1, arr2)
+  arr1 | arr2
+end
+
+# OR
+def merge(arr1, arr2)
+  (arr1 + arr2).uniq!
+end
+
+merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+# 6.7: HALVSIES
+
+# Write a method that takes an Array as an argument, and returns two Arrays (as a pair of nested Arrays) that contain the first half and second half of the original Array, respectively. If the original array contains an odd number of elements, the middle element should be placed in the first half Array.
+
+def halvsies(array)
+middle = (array.size / 2.0).ceil
+first_half = array.slice(0, middle)
+second_half = array.slice(middle, array.size - middle)
+[first_half, second_half]
+end
+
+
+halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+halvsies([5]) == [[5], []]
+halvsies([]) == [[], []]
+
+# 6.8: FIND THE DUPLICATE
+
+
+# Given an unordered array and the information that exactly one value in the array occurs twice (every other value occurs exactly once), how would you determine which value occurs twice? Write a method that will find and return the duplicate value that is known to be in the array.
+
+def find_dup(array)
+  new_array = []
+  array.each do |num|
+    if new_array.include?(num)
+      return num 
+    else
+    new_array << num 
+    end
+  end
+end
+
+# OR
+
+def find_dup(array)
+  array.find { |element| array.count(element) == 2 }
+end
+
+find_dup([1, 5, 3, 1]) == 1
+find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
+
+
+
+# 6.9: DOES MY LIST INCLUDE THIS?
+
+
+# Write a method named include? that takes an Array and a search value as arguments. This method should return true if the search value is in the array, false if it is not. You may not use the Array#include? method in your solution.
+
+def include?(array, value)
+  array.each { |element| return true if value == element }
+  false
+end
+
+# OR
+
+def include?(array, num)
+  array.count(num) > 0 ? true : false
+end
+
+include?([1,2,3,4,5], 3) == true
+include?([1,2,3,4,5], 6) == false
+include?([], 3) == false
+include?([nil], nil) == true
+include?([], nil) == false
+
+# 6.10: RIGHT TRIANGLES
+
+# Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+
+def triangle(num)
+  spaces = num - 1
+  stars = 1
+
+  num.times do |n|
+    puts (' ' * spaces) + ('*' * stars)
+    spaces -= 1
+    stars += 1
+  end
+end
+
+triangle(5)
+
+    *
+   **
+  ***
+ ****
+*****
+
+triangle(9)
+
+        *
+       **
+      ***
+     ****
+    *****
+   ******
+  *******
+ ********
+*********
